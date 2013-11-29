bacc0fdd0a3c7bf9303f33afac087e2c6496548d
Dykam <kramieb@gmail.com>
2013-11-29 01:47:35 +0100

    Added basic initialization to the UHCPVPManager and UHCUserManager.

--- src/com/ttaylorr/uhc/pvp/services/core/UHCPVPManager.java
+++ src/com/ttaylorr/uhc/pvp/services/core/UHCPVPManager.java
@@ -1,14 +1,25 @@
 package com.ttaylorr.uhc.pvp.services.core;
 
 import com.ttaylorr.uhc.pvp.Feature;
+import com.ttaylorr.uhc.pvp.services.CombatTagger;
+import com.ttaylorr.uhc.pvp.services.PVPRestrictionManager;
+import com.ttaylorr.uhc.pvp.services.SpawnManager;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import com.ttaylorr.uhc.pvp.services.PVPManager;
 
 public class UHCPVPManager implements PVPManager, Feature {
+  SpawnManager spawnManager;
+  PVPRestrictionManager pvpRestrictionManager;
+  CombatTagger combatTagger;
   @Override
   public void onEnable() {
-    //To change body of implemented methods use File | Settings | File Templates.
+    spawnManager = (SpawnManager)Bukkit.getServicesManager().getRegistration(SpawnManager.class);
+    pvpRestrictionManager = (PVPRestrictionManager)Bukkit.getServicesManager().getRegistration(PVPRestrictionManager.class);
+    combatTagger = (CombatTagger)Bukkit.getServicesManager().getRegistration(CombatTagger.class);
+
+    // TODO: Initialize the rest. Add listeners to track players.
   }
 
   @Override
--- src/com/ttaylorr/uhc/pvp/services/core/UHCUserManager.java
+++ src/com/ttaylorr/uhc/pvp/services/core/UHCUserManager.java
@@ -1,12 +1,20 @@
 package com.ttaylorr.uhc.pvp.services.core;
 
 import com.ttaylorr.uhc.pvp.Feature;
+import com.ttaylorr.uhc.pvp.services.LobbyManager;
+import com.ttaylorr.uhc.pvp.services.PVPManager;
 import com.ttaylorr.uhc.pvp.services.UserManager;
+import org.bukkit.Bukkit;
 
 public class UHCUserManager implements UserManager, Feature {
+  PVPManager pvpManager;
+  LobbyManager lobbyManager;
   @Override
   public void onEnable() {
-    //To change body of implemented methods use File | Settings | File Templates.
+    pvpManager = (PVPManager) Bukkit.getServicesManager().getRegistration(PVPManager.class);
+    lobbyManager = (LobbyManager)Bukkit.getServicesManager().getRegistration(LobbyManager.class);
+
+    // TODO: Initialize user manager. Add command listener (pvp leave, pvp join) etc.
   }
 
   @Override
