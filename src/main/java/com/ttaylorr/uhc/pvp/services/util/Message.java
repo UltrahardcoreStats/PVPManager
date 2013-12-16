6cb11aa4a80931a25c20519124d79bf44b888ed2
Dykam <kramieb@gmail.com>
2013-12-16 17:32:16 +0100

    Added standardized user messaging

--- src/main/java/com/ttaylorr/uhc/pvp/services/util/Message.java
+++ src/main/java/com/ttaylorr/uhc/pvp/services/util/Message.java
@@ -1,7 +1,20 @@
 package com.ttaylorr.uhc.pvp.services.util;
 
-/**
- * Created by Dykam on 16-12-13.
- */
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
 public class Message {
+    public static void message(CommandSender sender, String message) {
+        sender.sendMessage("[PVP] " + message);
+    }
+    public static void warn(CommandSender sender, String message) {
+        message(sender, ChatColor.GOLD + message);
+    }
+    public static void failure(CommandSender sender, String message) {
+        message(sender, ChatColor.RED + message);
+    }
+    public static void success(CommandSender sender, String message) {
+        message(sender, ChatColor.DARK_GREEN + message);
+    }
 }
