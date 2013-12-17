8b590af12f4e35d16cdeaaf7dde41302b2c1fe68
Dykam <kramieb@gmail.com>
2013-12-17 04:09:15 +0100

    Base file for UHC Game Modes, like in a lobby or in PVP

--- src/main/java/com/ttaylorr/uhc/pvp/services/core/UHCGameModeBase.java
+++ src/main/java/com/ttaylorr/uhc/pvp/services/core/UHCGameModeBase.java
@@ -1,7 +1,32 @@
 package com.ttaylorr.uhc.pvp.services.core;
 
-/**
- * Created by Dykam on 16-12-13.
- */
-public class UHCGameModeBase {
+import com.ttaylorr.uhc.pvp.PVPManagerPlugin;
+import com.ttaylorr.uhc.pvp.services.GameMode;
+import org.bukkit.entity.Player;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public abstract class UHCGameModeBase extends UHCServiceBase implements GameMode {
+    private Set<Player> players;
+    protected abstract void onEnter(Player p);
+    protected abstract boolean onExit(Player p);
+
+    protected UHCGameModeBase(PVPManagerPlugin plugin) {
+        super(plugin);
+        players = new HashSet<>();
+    }
+
+    @Override
+    public void enter(Player player) {
+        onEnter(player);
+        players.add(player);
+    }
+
+    @Override
+    public boolean exit(Player player) {
+        if(!onExit(player)) return false;
+        players.remove(player);
+        return true;
+    }
 }
