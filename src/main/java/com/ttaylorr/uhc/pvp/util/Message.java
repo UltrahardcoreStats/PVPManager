8d9c1bae11f321d7d5580b3afeb367551f5f6a72
ttaylorr <me@ttaylorr.com>
2013-12-16 22:40:38 -0500

    Add coloring to Message.message()

--- src/main/java/com/ttaylorr/uhc/pvp/util/Message.java
+++ src/main/java/com/ttaylorr/uhc/pvp/util/Message.java
@@ -5,7 +5,7 @@ import org.bukkit.command.CommandSender;
 
 public class Message {
     public static void message(CommandSender sender, String message) {
-        sender.sendMessage("[PVP] " + message);
+        sender.sendMessage(ChatColor.AQUA + "[PVP]" + ChatColor.GRAY + " - " + ChatColor.RESET + message);
     }
     public static void warn(CommandSender sender, String message) {
         message(sender, ChatColor.GOLD + message);
