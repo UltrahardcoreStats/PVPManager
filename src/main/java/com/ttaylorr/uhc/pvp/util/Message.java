f78ca17aa5dec5d64328e841377a78d32bd7d8d8
Dykam <kramieb@gmail.com>
2013-12-30 19:26:31 +0100

    Refactor Message to make line-wide colors be invasive. It uses a useful property of colors, which is that it acts like a RESET. Every RESET is simply swapped with the color provided.

--- src/main/java/com/ttaylorr/uhc/pvp/util/Message.java
+++ src/main/java/com/ttaylorr/uhc/pvp/util/Message.java
@@ -7,13 +7,20 @@ public class Message {
     public static void message(CommandSender sender, String message) {
         sender.sendMessage(ChatColor.AQUA + "[PVP]" + ChatColor.GRAY + " - " + ChatColor.RESET + message);
     }
+
+    public static void message(CommandSender sender, ChatColor color, String message) {
+        sender.sendMessage(ChatColor.AQUA + "[PVP]" + ChatColor.GRAY + " - " + ChatColor.RESET + colorize(color, message));
+    }
     public static void warn(CommandSender sender, String message) {
-        message(sender, ChatColor.GOLD + message);
+        message(sender, ChatColor.GOLD, message);
     }
     public static void failure(CommandSender sender, String message) {
-        message(sender, ChatColor.RED + message);
+        message(sender, ChatColor.RED, message);
     }
     public static void success(CommandSender sender, String message) {
-        message(sender, ChatColor.DARK_GREEN + message);
+        message(sender, ChatColor.DARK_GREEN, message);
+    }
+    private static String colorize(ChatColor color, String message) {
+        return color + message.replace(ChatColor.RESET.getChar(), color.getChar());
     }
 }
