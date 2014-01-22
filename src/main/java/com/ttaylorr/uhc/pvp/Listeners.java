6d7dead360120f2ed0e0451b17968cc84d5ae85d
Dykam <kramieb@gmail.com>
2014-01-22 21:32:44 +0100

    Prevent Enderpearl use outside of PVP.

--- src/main/java/com/ttaylorr/uhc/pvp/Listeners.java
+++ src/main/java/com/ttaylorr/uhc/pvp/Listeners.java
@@ -1,11 +1,13 @@
 package com.ttaylorr.uhc.pvp;
 
 import com.ttaylorr.uhc.pvp.core.UserManager;
+import com.ttaylorr.uhc.pvp.core.gamemodes.PVPGameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 
 public class Listeners implements Listener {
     private PVPManagerPlugin plugin;
@@ -24,6 +26,17 @@ public class Listeners implements Listener {
     @EventHandler
     private void onPlayerQuit(PlayerQuitEvent event) {
         userManager.unsubscribe(event.getPlayer());
+    }
+
+    @EventHandler
+    private void onPlayerTeleport(PlayerTeleportEvent event) {
+        if(event.getCause() != PlayerTeleportEvent.TeleportCause.ENDER_PEARL)
+            return;
+        UserManager.UserData userData = userManager.getUserData(event.getPlayer());
+        if(userData == null)
+            return;
+        if(!(userData.gameMode instanceof PVPGameMode))
+            event.setCancelled(true);
     }
 
 }
